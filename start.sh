41bb417d87eb02e440487f3aeb57a21d7fbffaa8
Ron Kurr <kurr@kurron.org>
2015-09-23 18:17:05 +0000

    Added JVM monitoring

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Use host networking to avoid having to map every hostname needed for the simulation
-docker run --detach=true --name=postgresql-service --net=host --publish=8400:8400 kurron/monitor-postgresql:latest
+docker run --detach=true --name=postgresql-service --net=host --publish=8400:8400 --env=NEW_RELIC_APP_NAME=postgresql-service --env=NEW_RELIC_HOST_DISPLAY_NAME=${HOSTNAME} kurron/monitor-postgresql:latest
 docker logs --follow=true --tail=25 postgresql-service
